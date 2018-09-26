3766eb4f26d2afbed7ad27e41202a00bd85d6f51
noxqs <targhan@gmail.com>
2018-09-26 13:51:55 +0800

    All bugs fixed, test scenarios working.

--- myscheduler.py
+++ myscheduler.py
@@ -1,37 +1,39 @@
 from datetime import datetime, timedelta
-import time
-from math import floor, ceil
+from math import floor
 
-# DAYS={1:"mon",2:"tue",3:"wed",4:"thu",5:"fri",6:"sat",7:"sun"}
 DAYS = {"mon": 0, "tue": 1, "wed": 2, "thu": 3, "fri": 4, "sat": 5, "sun": 6}
 DAYSS = dict((v, k) for k, v in DAYS.iteritems())
 
 
 class Job():
-    def __init__(self, start_time, end_time, days, interval, job, jobArgs=[]):
+    def __init__(self, start_time, end_time, days, interval, job, jobArgs=[], now=None):
+        self.now = now
         if isinstance(start_time, basestring):
             try:
                 start_time = datetime.strptime(start_time, "%H:%M:%S")
-                # start_time=time.strptime(start_time, "%H:%M:%S")
             except ValueError as e:
                 print "start time not in HH:MM:SS format."
         self.start_time = start_time
         if isinstance(end_time, basestring):
             try:
                 end_time = datetime.strptime(end_time, "%H:%M:%S")
-                # end_time=time.strptime(end_time, "%H:%M:%S")
             except ValueError as e:
                 print "end time not in HH:MM:SS format."
         self.end_time = end_time
         if isinstance(days, basestring):
             days = days.split(",")
 
-        self.days = days
         self.dayss = []
-        for day in days:
-            self.dayss.append(DAYSS[day])
+        self.days = []
+        self.id = None
 
-        print "dayss:", self.dayss
+        for day in days:
+            if isinstance(day, int):
+                self.dayss.append(DAYSS[day])
+                self.days.append(day)
+            elif isinstance(day, basestring):
+                self.dayss.append(day)
+                self.days.append(DAYS[day])
 
         if isinstance(interval, basestring):
             if interval.endswith("m"):
@@ -60,14 +62,23 @@ class Job():
     def info(self):
         st = self.start_time.strftime("%H:%M:%S")
         et = self.end_time.strftime("%H:%M:%S")
-        nr = self.next_run.strftime("%d/%m/%Y %H:%M:%S")
-        # st=time.strftime("%H:%M:%S", self.start_time)
-        # et=time.strftime("%H:%M:%S", self.end_time)
         interval = str(self.interval)
-        # days = ",".join(self.days)
-        # days=self.get_days_as_string()
-        print "On %s from %s till %s every %s sec." % (self.days, st, et, interval)
-        print "Next run:", nr, "on day",self.next_day
+        today_num = datetime.now().weekday()
+
+        dummy = "[%s] %s from %s till %s every %s sec run job %s(%s)" % (
+        self.group, ", ".join(self.dayss), st, et, interval, self.job.__name__, str(self.jobArgs))
+        dummy += ", Next run: %s (%s)" % (
+        self.next_run.strftime("%a %d-%m-%Y %H:%M:%S"), "this week" if today_num <= self.next_day else "next week")
+
+        if datetime.now().replace(year=1900, day=1, month=1) > self.start_time and self.next_day == datetime.now().weekday():
+            dummy += " [ACTIVE]"
+        else:
+            dummy += " [PENDING]"
+
+        if self.now is not None:
+            dummy += " TEST with now as %s" % (self.now)
+
+        return dummy
 
     def calc_next_run(self):
         now = datetime.now()
@@ -76,60 +87,48 @@ class Job():
         today = now.strftime("%a").lower()
         today_num = now.weekday()
 
-        # print "now:",nowtime
-        # print "st :",self.start_time
-        # print "et :",self.end_time
-        # print "int:",self.interval/60
-
-        # if today_num in self.days:
-        #     if nowtime > self.end_time:
-        #         print "already over"
-        #         return
-
-        ##################
-
         if nowtime > self.start_time:
-            print "already started"
             dt = nowtime - self.start_time
-            # print "dt:",dt.total_seconds()
             dtr = self.interval * (1 + floor(dt.total_seconds() / self.interval))
             nr = self.start_time + timedelta(seconds=dtr)
             print "Next run %s:" % today, nr
-            # print "overshot in sec:",dt.total_seconds()
-            # print "overshot round down:",dtr
             if nr > self.end_time:
-                print "next run overshoots end time"
+                print "WARN: next run overshoots end time"
             self.next_run = nr
         else:
-            print "not yet"
             self.next_run = self.start_time
-        # print "next run:", self.next_run
-
-        print "days:",self.days
-        print "today num:", today_num
-
-
 
         d2c_thisWeek = [d for d in self.days if d >= today_num]
         d2c_nextWeek = [d for d in self.days if d < today_num]
 
         self.next_day = None
 
-        if len(d2c_thisWeek)>0:
+        def calc_day(day, next_week=False):
+            print "next week:", next_week
+            print "day      :",day, DAYSS[day]
+            self.next_day = day
+            today = datetime.today()
+            if next_week:
+                _day = today + timedelta((day - today.weekday()) % 7)
+                self.next_run = self.next_run.replace(year=_day.year, day=_day.day, month=_day.month)
+            else:
+                _day = today + timedelta((day - today.weekday()) % 7)
+                self.next_run = self.next_run.replace(year=_day.year, day=_day.day, month=_day.month)
+
+        if len(d2c_thisWeek) > 0:
             if nowtime > self.end_time:
-                if len(d2c_thisWeek)>1:
-                    self.next_day=d2c_thisWeek[1]
+                if len(d2c_thisWeek) > 1:
+                    calc_day(next_week=False, day=d2c_thisWeek[1])
                 else:
                     if len(d2c_nextWeek) > 0:
-                        self.next_day = d2c_nextWeek[0]
+                        calc_day(next_week=True, day=d2c_nextWeek[0])
                     else:
                         print "No valid days"
-
             else:
-                self.next_day = d2c_thisWeek[0]
+                calc_day(next_week=False, day=d2c_thisWeek[0])
         else:
-            if len(d2c_nextWeek)>0:
-                self.next_day = d2c_nextWeek[0]
+            if len(d2c_nextWeek) > 0:
+                calc_day(next_week=True, day=d2c_nextWeek[0])
             else:
                 print "No valid days"
 
@@ -142,41 +141,64 @@ class MyScheduler:
         self.jobIndex = 0
         self.threaded = threaded
         self.jobs = {}
-        self.jobGroups = {}
+        self.jobsByGroups = {}
 
     def schedule(self, job, group=None):
         jobIndex = self.jobIndex
+        job.id = jobIndex
+
+        if group is None:
+            group = "default"
+
+        job.group = group
+
         self.jobs[jobIndex] = job
         self.jobIndex += 1
         if group is not None:
-            if group in self.jobGroups:
-                self.jobGroups[group].append(group)
+            if group in self.jobsByGroups:
+                self.jobsByGroups[group].append(job)
             else:
-                self.jobGroups[group] = [group]
+                self.jobsByGroups[group] = [job]
 
         return jobIndex
 
-    def clear(self, group=None, jobIndex=None):
-        if jobIndex is not None and jobIndex in self.jobs:
-            del self.jobs[jobIndex]
-            print "removed job Index", jobIndex
-            return
-        if group is not None and group in self.jobGroups:
-            print "removing all jobs in group", group
-            for job in self.jobGroups[group]:
-                del self.jobs[job]
-                print "removed job Index", jobIndex
-            del self.jobGroups[group]
+    def clear(self, group=None, id=None):
+        if id is not None and id in self.jobs:
+            del self.jobs[id]
+        elif group is not None and group in self.jobsByGroups:
+            for job in self.jobsByGroups[group]:
+                if job.id in self.jobs:
+                    del self.jobs[job.id]
+            self.jobsByGroups[group]=[]
 
     def run_pending(self):
         for i, job in self.jobs.iteritems():
             print i, job.start
 
+    def info(self):
+        if len(self.jobs):
+            print "[ JOBS at %s]" % datetime.now().strftime("%a %d-%m-%Y %H:%M:%S")
+            for id, job in self.jobs.iteritems():
+                if id != job.id:
+                    print "! %3d %s" % (id, job.info())
+                else:
+                    print "  %3d %s" % (id, job.info())
+        else:
+            print "No jobs."
+
 def test_job():
     print "run test job"
 
+
 if __name__ == '__main__':
     schedular = MyScheduler()
-
-    job1 = Job(start_time="17:50:00", end_time="22:50:00", days=[6, 0], interval="5m", job=test_job, jobArgs="aap")
-    job1.info()
+    job1 = Job(start_time="11:50:00", end_time="22:50:00", days=["wed", "tue"], interval="5m", job=test_job,
+               jobArgs="aap", now="09-25-2018 17:50:00")
+    job2 = Job(start_time="17:50:00", end_time="22:50:00", days=["mon","tue"], interval="5m", job=test_job, jobArgs="aap", now="09/25/2018 17:50:00")
+    schedular.schedule(job1, group="camera1")
+    schedular.schedule(job2, group="camera2")
+    schedular.info()
+    schedular.clear(group="camera1")
+    schedular.info()
+    schedular.clear(id=1)
+    schedular.info()
