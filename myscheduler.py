28300d19af357d1bab8b8d1b0df9258900d2cb5f
noxqs <targhan@gmail.com>
2018-09-27 11:34:55 +0800

    Getting ready for production.

--- myscheduler.py
+++ myscheduler.py
@@ -1,13 +1,16 @@
+import threading
+import time
 from datetime import datetime, timedelta
 from math import floor
 
 DAYS = {"mon": 0, "tue": 1, "wed": 2, "thu": 3, "fri": 4, "sat": 5, "sun": 6}
 DAYSS = dict((v, k) for k, v in DAYS.iteritems())
 
-
 class Job():
     def __init__(self, start_time, end_time, days, interval, job, jobArgs=[], now=None):
         self.now = now
+        self.job_thread = None
+
         if isinstance(start_time, basestring):
             try:
                 start_time = datetime.strptime(start_time, "%H:%M:%S")
@@ -84,28 +87,23 @@ class Job():
         now = datetime.now()
         self.next_run = now
         nowtime = datetime.now().replace(year=1900, day=1, month=1)
-        today = now.strftime("%a").lower()
         today_num = now.weekday()
 
         if nowtime > self.start_time:
             dt = nowtime - self.start_time
             dtr = self.interval * (1 + floor(dt.total_seconds() / self.interval))
             nr = self.start_time + timedelta(seconds=dtr)
-            print "Next run %s:" % today, nr
-            if nr > self.end_time:
-                print "WARN: next run overshoots end time"
+            # if nr > self.end_time:
+            #     print "WARN: next run overshoots end time"
             self.next_run = nr
         else:
             self.next_run = self.start_time
 
         d2c_thisWeek = [d for d in self.days if d >= today_num]
         d2c_nextWeek = [d for d in self.days if d < today_num]
-
         self.next_day = None
 
         def calc_day(day, next_week=False):
-            print "next week:", next_week
-            print "day      :",day, DAYSS[day]
             self.next_day = day
             today = datetime.today()
             if next_week:
@@ -137,11 +135,12 @@ class Job():
 
 
 class MyScheduler:
-    def __init__(self, threaded=False):
+    def __init__(self, threaded=False, skip_unfinished_jobs=True):
         self.jobIndex = 0
         self.threaded = threaded
         self.jobs = {}
         self.jobsByGroups = {}
+        self.skip_unfinished_jobs = skip_unfinished_jobs
 
     def schedule(self, job, group=None):
         jobIndex = self.jobIndex
@@ -162,6 +161,21 @@ class MyScheduler:
 
         return jobIndex
 
+    def run_job(self, id):
+        if self.skip_unfinished_jobs:
+            if self.jobs[id].job_thread is None:
+                self.jobs[id].job_thread = threading.Thread(target=self.jobs[id].job, args=[self.jobs[id].jobArgs])
+                self.jobs[id].job_thread.start()
+            else:
+                if not self.jobs[id].job_thread.isAlive():
+                    self.jobs[id].job_thread = threading.Thread(target=self.jobs[id].job, args=[self.jobs[id].jobArgs])
+                    self.jobs[id].job_thread.start()
+                else:
+                    print "WARN: Skipping job.id", id
+        else:
+            dontcare = threading.Thread(target=self.jobs[id].job, args=[self.jobs[id].jobArgs])
+            dontcare.start()
+
     def clear(self, group=None, id=None):
         if id is not None and id in self.jobs:
             del self.jobs[id]
@@ -170,6 +184,31 @@ class MyScheduler:
                 if job.id in self.jobs:
                     del self.jobs[job.id]
             self.jobsByGroups[group]=[]
+
+    def start(self):
+        if self.threaded:
+            self._mainThread = threading.Thread(target=self._threaded)
+            self._mainThread.start()
+        else:
+            self._threaded()
+
+    def stop(self):
+        self.running=False
+        if self.threaded:
+            self._mainThread.join()
+
+    def check_jobs(self):
+        for id, job in self.jobs.iteritems():
+            # print id,job.next_run, datetime.now(), job.next_run < datetime.now()
+            if job.next_run < datetime.now():
+                self.run_job(id)
+                job.calc_next_run()
+
+    def _threaded(self):
+        self.running = 1
+        while self.running:
+            time.sleep(1)
+            self.check_jobs()
 
     def run_pending(self):
         for i, job in self.jobs.iteritems():
@@ -186,19 +225,19 @@ class MyScheduler:
         else:
             print "No jobs."
 
-def test_job():
-    print "run test job"
-
+def test_job(param):
+    print ">> starting %s"%param
 
 if __name__ == '__main__':
-    schedular = MyScheduler()
-    job1 = Job(start_time="11:50:00", end_time="22:50:00", days=["wed", "tue"], interval="5m", job=test_job,
-               jobArgs="aap", now="09-25-2018 17:50:00")
-    job2 = Job(start_time="17:50:00", end_time="22:50:00", days=["mon","tue"], interval="5m", job=test_job, jobArgs="aap", now="09/25/2018 17:50:00")
+    schedular = MyScheduler(threaded=True, skip_unfinished_jobs = True)
+    job1 = Job(start_time="9:50:00", end_time="11:33:00", days=["thu", "tue"], interval="3s", job=test_job, jobArgs="3sec job", now="09-25-2018 17:50:00")
+    job2 = Job(start_time="11:33:00", end_time="11:34:00", days=["thu", "tue"], interval="5s", job=test_job, jobArgs="5sec job", now="09-25-2018 17:50:00")
+    job3 = Job(start_time="11:34:00", end_time="11:35:00", days=["thu", "tue"], interval="8s", job=test_job, jobArgs="8sec job", now="09-25-2018 17:50:00")
     schedular.schedule(job1, group="camera1")
-    schedular.schedule(job2, group="camera2")
+    schedular.schedule(job2, group="camera1")
+    schedular.schedule(job3, group="camera1")
     schedular.info()
-    schedular.clear(group="camera1")
-    schedular.info()
-    schedular.clear(id=1)
-    schedular.info()
+    schedular.start()
+    time.sleep(3000000)
+    schedular.stop()
+    print "Done."
